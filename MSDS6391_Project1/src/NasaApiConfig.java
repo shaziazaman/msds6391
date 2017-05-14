24753714927a2037d41f1aba1fb35050500e7bd1
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-05-14 13:41:10 -0500

    Updated the code to retrieve all the images and display
    Updated the code to retrieve all the images and display

--- MSDS6391_Project1/src/NasaApiConfig.java
+++ MSDS6391_Project1/src/NasaApiConfig.java
@@ -1,20 +1,67 @@
 
 public class NasaApiConfig {
-	String apiKey = "9Ihs7mZs2OkFpVeNHaRF76mnDZ02lOSZoe2NIeeh";
-	String apiUrl = "https://api.nasa.gov/EPIC/api/natural/images?api_key=" + apiKey;
+	String apiKey = "?api_key=9Ihs7mZs2OkFpVeNHaRF76mnDZ02lOSZoe2NIeeh";
+	String apiBaseUrl = "https://api.nasa.gov/EPIC/api/natural/images";
 	String imageType = "natural";
 	String imageFileType = "png";
 	String imageFileName = "epic_1b_20170510190006_02";
 	String imageFileYear = "2017";
 	String imageFileMonth = "05";
 	String imageFileDay = "10";
-	String imageUrl = "https://api.nasa.gov/EPIC/archive/" + imageType + "/" +
-			imageFileYear + "/" + imageFileMonth + "/" + imageFileDay + "/" +
-			imageFileType + "/" + imageFileName + "." + imageFileType + 
-			"?api_key=" + apiKey;
+	String imageBaseUrl = "https://api.nasa.gov/EPIC/archive/";
 	
 	public NasaApiConfig() {
 		super();
 	}
 	
+	// This method will be used to generate apiURL to retrieve information about
+	// lated images of Earth being captured by NASA
+	public String getDerivedApiURL() {
+		return apiBaseUrl + apiKey;
+	}
+	
+	// This method will be used to generate image URL based on the class data members
+	// that is already being set in the class
+	public String getDerivedImageURL() {
+		// create image URL based on parameter being set in class
+		return imageBaseUrl + imageType + "/" +
+				imageFileYear + "/" + imageFileMonth + "/" + imageFileDay + "/" +
+				imageFileType + "/" + imageFileName + "." + imageFileType + apiKey;
+	}
+
+	// Method overloading for getDerivedImageURL with extra parameters
+	// This method will be used to reset the image Date and File name information
+	// And then generate new image URL based on these variables
+	
+	public String getDerivedImageURL(String imageDateString, String imageName) {
+		// imageDateString is given in format as 2017-05-10 22:11:41
+		// date and time part are separated by white space
+		
+		// use split functionality to break date string into date part and time part
+		String[] dateTimeToken = imageDateString.split(" ");
+		// capture the date part only for further processing
+		String imageDatePart = dateTimeToken[0];
+		
+		// in date part, year, month and date are separated by -
+		// use split functionality again to get year, month and date separately
+		String[] dateTokens = imageDatePart.split("-");
+		
+		// year is display first, so it is captured at index 0 of the list
+		// save this year as the class data member imageFileYear
+		this.imageFileYear = dateTokens[0];
+		// similarly, month is displayed next, and it is captured at index 1
+		// save this month as the class data member imageFileMonth
+		this.imageFileMonth = dateTokens[1];
+		// day of month is displayed last, and it is captured at index 2
+		// save this year as the class data member imageFileDay
+		this.imageFileDay = dateTokens[2];
+		
+		// save the imageName being passed to the method as class member imageFileName
+		this.imageFileName = imageName;
+		
+		// As imageFileName, imageFileYear, imageFileMonth and imageFileDay are being reset with new values
+		// calling the method getDerivedImageURL without any parameters will used new values of these variables
+		// to create new URL to retrieve image
+		return getDerivedImageURL();
+	}
 }
--- MSDS6391_Project1/src/PlanetDemo.java
+++ MSDS6391_Project1/src/PlanetDemo.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 public class PlanetDemo extends PApplet {
+	// NasaApiConfig will be used get set and get specific url for NASA Api
 	NasaApiConfig nasaApiConfig  = new NasaApiConfig();
-	PImage img;
+	
+	// imageList will hold all the images being retrieved from NASA
+	ArrayList<PImage> imageList = new ArrayList<>();
+	
+	// imageIndex will be used by imageList to retrieve image at specific index
+	// it will be reset in draw method when last image from the listhas been displayed to start over
+	int imageIndex = 0;
 	
 	static public void main(String[] passedArgs){
 		String[] appletArgs = new String[] {"PlanetDemo"};
@@ -22,30 +32,49 @@ public class PlanetDemo extends PApplet {
 
 	@Override
 	public void settings() {
-		super.settings();
 		size(700, 700, P3D);
 	}
 
 
 	@Override
 	public void setup() {
-		callApi(nasaApiConfig.apiUrl);
-		System.out.println("Image link:" + nasaApiConfig.imageUrl);
-		img = loadImage(nasaApiConfig.imageUrl, nasaApiConfig.imageFileType);
+		// retrieve the information about latest images of earth captured by NASA
+		// the result will be saved as JSONArray
+		JSONArray jsonList = loadJSONArray(nasaApiConfig.getDerivedApiURL());
+		// print the response received from call to NASA API
+		System.out.println(jsonList.toString());
+		// iterate through JSONArray to get information about each image
+		for(int index = 0; index < jsonList.size(); index++) {
+			JSONObject jsonObject = jsonList.getJSONObject(index);
+			// capture the date when image was taken by NASA
+			String imageDateString =  jsonObject.getString("date");
+			// capture the image name
+			String imageName = jsonObject.getString("image");
+			// retrieve specific image from NASA based on image date and name
+			PImage img = loadImage(nasaApiConfig.getDerivedImageURL(imageDateString, imageName), nasaApiConfig.imageFileType);
+			// print the link being used to retrieve image from NASA
+			System.out.println("Image link: " + nasaApiConfig.getDerivedImageURL());
+			// Resize the image to fit in display window
+			img.resize(width, height);
+			// Add the image to image List, it will be used later to display image
+			imageList.add(img);
+		}
+		
 	}
 	
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		super.draw();
-		img.resize(width, height);
-		image(img, 0, 0);
+		// iterate over image list to display new image every time
+		image(imageList.get(imageIndex), 0, 0);
+		// increase the imageIndex to retrieve new image next time in draw
+		imageIndex = imageIndex + 1;
+		// if imageIndex exceed the size of image List 
+		// then reset to start displaying images from beginning of the list
+		if(imageIndex >= imageList.size()){
+			imageIndex = 0;
+		}
+		// add delay for 250 millisecond to redraw the window
+		delay(250);
 	}
 
-	public void callApi(String apiUrl) {
-		JSONArray jsonList = loadJSONArray(apiUrl);
-		System.out.println(jsonList.toString());
-	}
-	
-	
 }
