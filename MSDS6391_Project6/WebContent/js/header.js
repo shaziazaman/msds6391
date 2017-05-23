be42e11990e2a9657a78452b8a048c19d747b0ed
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-05-23 13:07:38 -0500

    Updated link for About SMU MSDS page and personal page for szaman
    Updated link for About SMU MSDS page and personal page for szaman

--- MSDS6391_Project6/WebContent/js/header.js
+++ MSDS6391_Project6/WebContent/js/header.js
@@ -5,11 +5,17 @@
 var headerString = "<header>\n" +
 		"<section>\n" +
 		"<nav>\n" +
-		"<a href=\"index.html\"><b>Home</b></a>\t<a href=\"#\"><b>About SMU MSDS</b></a>\t<a href=\"contactus.html\"><b>Contact Us</b></a>\n" +
+		"<a href=\"index.html\"><b>Home</b></a>\t" +
+		"<a href=\"https://datascience.smu.edu\" onclick=\"redirectAlert()\"><b>About SMU MSDS</b></a>\t" +
+		"<a href=\"contactus.html\"><b>Contact Us</b></a>\n" +
 		"</nav>\n" +
 		"</section>\n" +
 		"</header>";
 
 function createHeader(){
 	document.write(headerString);
+}
+
+function redirectAlert(){
+	alert("Leaving SMU SMDS Students Personal pages. You will be redirected to SMU MSDS website.")
 }
