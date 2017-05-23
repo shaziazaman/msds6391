2c1af2231155194c1545ed746db1f7f80f5d2e6e
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-05-22 20:14:28 -0500

    2nd draft with templates to fill-in
    2nd draft with templates to fill-in

---
+++ MSDS6391_Project6/WebContent/js/header.js
@@ -0,0 +1,15 @@
+/**
+ * Header content
+ */
+
+var headerString = "<header>\n" +
+		"<section>\n" +
+		"<nav>\n" +
+		"<a href=\"index.html\"><b>Home</b></a>\t<a href=\"#\"><b>About SMU MSDS</b></a>\t<a href=\"contactus.html\"><b>Contact Us</b></a>\n" +
+		"</nav>\n" +
+		"</section>\n" +
+		"</header>";
+
+function createHeader(){
+	document.write(headerString);
+}
