4170c03c23780d67b6f9ee4b5cea55e869dd0d68
rlorenza <rlorenza94@gmail.com>
2017-05-30 14:42:48 -0700

    Added other cube images to cube.js and aligned xy coordiantes.
    Added other cube images to cube.js and aligned xy coordiantes. Also fixed one of the missing images that had wrong name.

--- MSDS6391_Project7/WebContent/js/cubes.js
+++ MSDS6391_Project7/WebContent/js/cubes.js
@@ -7,16 +7,38 @@ var  angle=0;
 
 function setup() {
    createCanvas(1500,1000,WEBGL);
-   //background(255);
+    //displayText();
 }
 
 function draw() {
-	 background(51);
-	 translate(0,0,mouseX);
+	 background(50);
+	 push();
+	 translate(0,-120,0);
 	 rotateX(angle);
-	 rotateY(angle*.06);
+	 rotateY(angle*.08);
 	 box(200);
-	 angle +=0.1;
+	 angle +=0.12;
+	 pop();
+	 push();
+	 translate(-width/3,-120,0);
+	 rotateX(angle);
+	 rotateY(angle*.08);
+	 box(200);
+	 angle +=0.12;
+	 pop();
+	 translate(width/3,-120,0);
+	 rotateX(angle);
+	 rotateY(angle*.08);
+	 box(200);
+	 angle +=0.12;	 
 	   
 }//close draw
+
+function displayText(){
+	  textSize(20);
+	  fill(255)
+	  text("Cube Visualization Using WEBGL",650,200); 
+	  text("3D Rendering in P5 ",470,80);
+	 
+	}
 
