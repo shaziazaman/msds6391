ad17bb49dc7a4555cce2db3ccec315366176ec17
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-06-05 12:00:16 -0500

    Commiting changes so far to parse json object
    Commiting changes so far to parse json object

--- MSDS6391_Project8/WebContent/js/weather.js
+++ MSDS6391_Project8/WebContent/js/weather.js
@@ -3,9 +3,11 @@
  */
 
 var apiid = '0b55e5f12b5583816c9356d808f18249'
+var city = '';
 function gettingJSON(){
 	document.write("jquery loaded");
-	var jsondata = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&appid=0b55e5f12b5583816c9356d808f18249",function(json){
-	$(document.body).append(jsondata.responseText);
+	$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiid,function(json){
+	city = json.name;
+	$(document.body).append('City: ' + city);
 })
 }
