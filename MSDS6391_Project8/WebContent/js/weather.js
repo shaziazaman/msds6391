7366d5d5b5239e4cded9f49b875e484ef8429e64
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-06-05 22:52:16 -0500

    Updating code by captured variables from weather json as global variables
    Updating code by captured variables from weather json as global
    variables

--- MSDS6391_Project8/WebContent/js/weather.js
+++ MSDS6391_Project8/WebContent/js/weather.js
@@ -4,10 +4,36 @@
 
 var apiid = '0b55e5f12b5583816c9356d808f18249'
 var city = '';
+var lat = 0;
+var lon = 0;
+var condition = '';
+var condition = '';
+var temp = 0;
+var pressure = 0;
+var humidity = 0;
+var temp_min = 0;
+var temp_max = 0;
+var wind_speed = 0;
+var wind_degree = 0;
+var sunrise = 0;
+var sunset = 0;
+var country = '';
 function gettingJSON(){
 	document.write("jquery loaded");
 	$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=London&appid=' + apiid,function(json){
 	city = json.name;
-	$(document.body).append('City: ' + city);
+	lat = json.coord.lat;
+	lon = json.coord.lon;
+	condition = json.weather[0].main;
+	temp = json.main.temp;
+	pressure = json.main.pressure;
+	humidity = json.main.humidity;
+	temp_min = json.main.temp_min;
+	temp_max = json.main.temp_max;
+	wind_speed = json.wind.speed;
+	wind_degree = json.wind.deg;
+	sunrise = json.sys.sunrise;
+	sunset = json.sys.sunset;
+	country = json.sys.country;
 })
 }
