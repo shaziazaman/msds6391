846007ac5de3780bf21797ab15da26dbf5e59ee5
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-06-13 05:56:10 -0500

    update the code for weather_d3.html
    update the code for weather_d3.html

--- MSDS6391_Project8/WebContent/js/weather_d3.js
+++ MSDS6391_Project8/WebContent/js/weather_d3.js
@@ -1,108 +1,171 @@
 /**
- * 
+ * D3 script for weather data
  */
 
 var apiid = '0b55e5f12b5583816c9356d808f18249'
-/* var mapapiid = 'AIzaSyDcbIf1hGXMTXaAFzSEGz8CHL6zlUs_h9k'; */
 var mapapiid = 'AIzaSyBsJmm8GAuulHcRxYk3X3E5ZYWXObQvEWU';
 
-var city = '';
-var lat = 0;
-var lon = 0;
-var condition = '';
-var condition = '';
-var temp = 0;
-var pressure = 0;
-var humidity = 0;
-var temp_min = 0;
-var temp_max = 0;
-var wind_speed = 0;
-var wind_degree = 0;
-var sunrise = 0;
-var sunset = 0;
-var country = '';
-var units = 'imperial'
-var temp_scale = 'Fahrenheit'
-function gettingJSON(cityName, unitSelected){
-	city = cityName;
+var weatherData = {};
+weatherData.city = 'Dallas';
+weatherData.lat = 0;
+weatherData.lon = 0;
+weatherData.condition = '';
+weatherData.temp = 0;
+weatherData.pressure = 0;
+weatherData.humidity = 0;
+weatherData.temp_min = 0;
+weatherData.temp_max = 0;
+weatherData.wind_speed = 0;
+weatherData.wind_degree = 0;
+weatherData.sunrise = 0;
+weatherData.sunset = 0;
+weatherData.country = '';
+units = 'imperial'
+temp_scale = 'Fahrenheit'
+
+var forecastData = {};
+forecastData.city = 'Dallas';
+forecastData.lat = 0;
+forecastData.lon = 0;
+forecastData.country = '';
+forecastData.days = [];
+
+var img_url = '';
+
+function loadWeatherData(cityName, unitSelected) {
+	weatherData.city = cityName;
 	units = unitSelected;
-	$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=' + units +'&appid=' + apiid,function(json){
-	city = json.name;
-	lat = json.coord.lat;
-	lon = json.coord.lon;
-	condition = json.weather[0].main;
-	temp = json.main.temp;
-	pressure = json.main.pressure;
-	humidity = json.main.humidity;
-	temp_min = json.main.temp_min;
-	temp_max = json.main.temp_max;
-	wind_speed = json.wind.speed;
-	wind_degree = json.wind.deg;
-	sunrise = json.sys.sunrise;
-	sunset = json.sys.sunset;
-	country = json.sys.country;
-	
-	if(units == 'imperial') {
-		temp_scale = 'Fahrenheit'
+	d3.json('http://api.openweathermap.org/data/2.5/weather?q=' + weatherData.city + '&units=' + units +'&appid=' + apiid, function (jsondata) {
+    
+		console.log(jsondata);
+
+		weatherData.city = jsondata.name;
+		weatherData.lat = jsondata.coord.lat;
+		weatherData.lon = jsondata.coord.lon;
+		weatherData.condition = jsondata.weather[0].main;
+		weatherData.temp = jsondata.main.temp;
+		weatherData.pressure = jsondata.main.pressure;
+		weatherData.humidity = jsondata.main.humidity;
+		weatherData.temp_min = jsondata.main.temp_min;
+		weatherData.temp_max = jsondata.main.temp_max;
+		weatherData.wind_speed = jsondata.wind.speed;
+		weatherData.wind_degree = jsondata.wind.deg;
+		weatherData.sunrise = jsondata.sys.sunrise;
+		weatherData.sunset = jsondata.sys.sunset;
+		weatherData.country = jsondata.sys.country;
+
+		loadWeatherTable(weatherData);
+    	img_url = generateImageUrl(12);
+    	loadGoogleImage(img_url);
+    	loadForecastData();
+		loadForecastTable(forecastData);
+    });
+}
+
+function loadForecastData() {
+	d3.json('http://api.openweathermap.org/data/2.5/forecast?q=' + forecastData.city + '&units=' + units +'&appid=' + apiid, function (jsondata) {
+    
+		console.log(jsondata);
+
+		forecastData.city = jsondata.city.name;
+		forecastData.country = jsondata.city.country;
+		forecastData.lat = jsondata.city.coord.lat;
+		forecastData.lon = jsondata.city.coord.lon;
+		// Need to loop through the list and add flatten out data
+		// to forecastData list
+//		forecastData.days = jsondata.list;
+    });
+}
+
+function generateImageUrl(zoom_value) {
+    var latlon = weatherData.lat + "," + weatherData.lon;
+
+    return img_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+latlon+'&zoom=' +zoom_value+ '&size=490x340&sensor=false&key=' + mapapiid;
+
+}
+
+function loadGoogleImage(url) {
+	console.log("adding image")
+	svg = d3.select("body")
+			.select("svg#gmap")
+			.append("image")
+			.attr("width","490")
+			.attr("height","360")
+			.attr("xlink:href", url);
+}
+
+function loadWeatherTable(data) {
+	console.log("adding table");
+
+	//transpose data as array
+	var transposeData = transposeWeatherDataIntoArray(weatherData);
+
+	//get column list
+	var columns = Object.keys(transposeData[0]);
+
+	// remove table before appending new table
+	d3.select("body").select("div#wtable").selectAll("table").remove();
+
+	// add new table
+	var table = d3.select("body").select("div#wtable").append("table");
+
+	var thead = table.append("thead");
+	var tbody = table.append("tbody");
+
+	thead.append('tr')
+		  .selectAll('th')
+		  .data(columns).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
+
+	// create a row for each object in the data
+	var rows = tbody.selectAll('tr')
+	  .data(transposeData)
+	  .enter()
+	  .append('tr');
+
+	// create a cell in each row for each column
+	var cells = rows.selectAll('td')
+	  .data(function (row) {
+		return columns.map(function (column) {
+		  return {column: column, value: row[column]};
+		});
+	  })
+	  .enter()
+	  .append('td')
+		.text(function (d) { return d.value; });
+}
+
+function transposeWeatherDataIntoArray(data) {
+	var tdata = [];
+	var types = Object.keys(data);
+
+	for(i=0; i < types.length; i++) {
+		var type = types[i];
+		var value = data[type];
+		var obj = {};
+		obj.Type = type;
+		obj.Value = value;
+		tdata.push(obj);
 	}
-	else if (units == 'metric') {
-		temp_scale = 'Celsius'
-	}
-	
-	refreshValuesOnPage();
-	showThermometer();
-	showMap(12);
-})
+	console.log(tdata);
+	return tdata;
 }
 
-function refreshValuesOnPage() {
-	document.getElementById("city").innerHTML = city;
-	document.getElementById("temp").innerHTML = temp;
-	document.getElementById("pressure").innerHTML = pressure;
-	document.getElementById("humidity").innerHTML = humidity;
-	document.getElementById("temp_min").innerHTML = temp_min;
-	document.getElementById("temp_max").innerHTML = temp_max;
-	document.getElementById("wind_speed").innerHTML = wind_speed;
-	document.getElementById("wind_degree").innerHTML = wind_degree;
-	document.getElementById("sunrise").innerHTML = sunrise;
-	document.getElementById("sunset").innerHTML = sunset;
-}
+function loadForecastTable(data) {
 
-function showThermometer() {
-	var cvx = document.getElementById("thermocanvas");
-	var ctx = cvx.getContext("2d");
-	ctx.clearRect(0, 0, cvx.width, cvx.height);
-	RGraph.reset(cvx);
-	var thermometer = new RGraph.Thermometer({
-        id: 'thermocanvas',
-        min: -14,
-        max: 120,
-        value: temp,
-        options: {
-            gutterLeft: 25,
-            gutterRight: 25,
-            colors: ['rgba(255,0,0,1)'],
-            bulbBottomRadius: 40,
-            titleSide: temp_scale
-        }
-        
-    }).draw();
-}
+	// remove table before appending new table
+	d3.select("body").select("div#ftable").selectAll("table").remove();
 
-function showMap(zoom_value) {
-    var latlon = lat + "," + lon;
+	// add new table
+	var table = d3.select("body").select("div#ftable").append("table");
 
-    var img_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+latlon+'&zoom=' +zoom_value+ '&size=490x340&sensor=false&key=' + mapapiid;
+	var thead = table.append("thead");
+	var tbody = table.append("tbody");
 
-    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-}
-
-function getFormattedTime(timeInMilleseconds) {
-	var d = new Date(timeInMilleseconds);
-	return d.toUTCString();
-}
-
-window.onload = function () {
-	showThermometer();
-	showMap(1);
+	thead.append('tr')
+		  .selectAll('th')
+		  .data(['day1','day2','day3', 'day4', 'day5']).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
 }
