4510ae5847e04b4469a9afca55735d49a946425e
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-07-04 23:53:49 -0500

    fix the date stamp on line chart
    fix the date stamp on line chart

--- MSDS6391_Project8/WebContent/js/weather_d3.js
+++ MSDS6391_Project8/WebContent/js/weather_d3.js
@@ -267,7 +267,7 @@ function render(data,x_range,y_range,m,w,h,x_dim_accessor,y_dim_accessor){
       graph.append("text").style("font-size", "16px").text("Five Day Forecast");
 	
 	  // get datatime labels for x-axis
-	  var x_labels = data.map(function(d) {return d.dt_txt;});
+	  var x_labels = data.map(function(d) {return d.dt_txt.substring(0,13);});
       // create yAxis
       var x_txt = d3.scale.ordinal()
     			.domain(x_labels)
