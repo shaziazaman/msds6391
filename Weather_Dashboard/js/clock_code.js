32c2d1aaa52a7ae69ea1febdc88b2ebaad003860
Shazia Zaman <shazia.a.zaman@gmail.com>
2017-07-18 19:57:30 -0500

    first checkin for standalone weather app
    first checkin for standalone weather app

---
+++ Weather_Dashboard/js/clock_code.js
@@ -0,0 +1,243 @@
+// Reference: http://bl.ocks.org/tomgp/6475678
+
+var timezoneapiid = 'AIzaSyDefPXemHwxIfnWIg7D2ZpZdq7fjiXvUBc';
+var radians = 0.0174532925;
+var clockRadius = 80;
+var clockMargins = 15;
+var clockWidth = (clockRadius+clockMargins)*2;
+var clockHeight = (clockRadius+clockMargins)*2;
+var sunriseHandLength = 2*clockRadius/3;
+var sunsetHandLength = 2*clockRadius/3;
+var hourTickStart = clockRadius + 10;
+var hourTickLength = -1;
+var hourLabelRadius = clockRadius - 10;
+var hourLabelYOffset = 7;
+
+var hourScale = d3.scale.linear()
+	.range([0,354])
+	.domain([0,24]);
+
+var handData = [
+	{
+		type:'sunrise-hour',
+		value:0,
+		length:-sunriseHandLength,
+		scale:hourScale
+	},
+	{
+		type:'sunset-hour',
+		value:0,
+		length:-sunsetHandLength,
+		scale:hourScale
+	}
+];
+
+var radioLabels = ['Table','Clock'];
+
+function displayClockOrTable(data, coordinate, dashboard_svg){
+
+	var latlon = coordinate.lat + ',' + coordinate.lon;
+	var timeInSecond = data.time;
+	var svg = dashboard_svg.append("g");
+	svg.attr("id","clock");
+	
+	d3.json('https://maps.googleapis.com/maps/api/timezone/json?location='+latlon+'&timestamp='+timeInSecond+'&key='+timezoneapiid,function (jsondata) {
+		console.log('timezone data', jsondata);
+		var offset = {};
+		offset.dst = jsondata.dstOffset;
+		offset.raw = jsondata.rawOffset;
+
+		console.log('offset data', offset);
+		drawDayLightClock(data, offset, svg);
+		loadClockTableGroup(data, offset, svg);
+	});
+
+}
+
+
+function convertToUTCDateString(dateNumber, offset) {
+	var timeData =  convertToUTCDate(dateNumber, offset);
+	var pad = '00';
+	return (pad + timeData.hours).slice(-2) + ":" + (pad + timeData.minutes).slice(-2) + ":" + (pad + timeData.seconds).slice(-2);
+}
+
+function convertToUTCDate(dateNumber, offset) {
+	var timeData = {};
+	var convertedDate =  new Date(dateNumber * 1000);
+	convertedDate.setTime( convertedDate.getTime() + (offset.raw + offset.dst)*1000 );
+	timeData.hours = convertedDate.getUTCHours();
+	timeData.minutes = convertedDate.getUTCMinutes();
+	timeData.seconds = convertedDate.getUTCSeconds();
+
+	console.log('time data', timeData);
+
+	return timeData;
+}
+
+function drawDayLightClock(data, offset, svg) {
+
+	svg.append('g')
+		.append("text")
+		.attr("x", 15)
+		.attr("y", 15)
+		.attr("font-size",14)
+		.attr("font-weight","bold")
+		.text('Sunrise and Sunset Time');
+
+    var face = svg.append('g')
+		.attr('id','clock_face')
+		.attr('transform','translate(' + (clockRadius + clockMargins) + ',' + (clockRadius + clockMargins + 30) + ')');
+
+	face.selectAll('.hour-tick')
+		.data(d3.range(0,24)).enter()
+			.append('line')
+			.attr('class', 'hour-tick')
+			.attr('x1',0)
+			.attr('x2',0)
+			.attr('y1',hourTickStart)
+			.attr('y2',hourTickStart + hourTickLength)
+			.attr('transform',function(d){
+				return 'rotate(' + hourScale(d) + ')';
+			});
+
+	face.selectAll('.hour-label')
+		.data(d3.range(3,25,3))
+			.enter()
+			.append('text')
+			.attr('class', 'hour-label')
+			.attr('text-anchor','middle')
+			.attr('x',function(d){
+				return hourLabelRadius*Math.sin(hourScale(d)*radians);
+			})
+			.attr('y',function(d){
+				return -hourLabelRadius*Math.cos(hourScale(d)*radians) + hourLabelYOffset;
+			})
+			.text(function(d){
+				return d;
+			});
+
+	face.append('g').attr('id','face-overlay')
+		.append('circle').attr('class','hands-cover')
+			.attr('x',0)
+			.attr('y',0)
+			.attr('r',clockRadius/20);
+
+	var hands = face.append('g').attr('id','clock-hands');
+
+	hands.selectAll('line')
+		.data(handData)
+		.enter()
+		.append('line')
+		.attr('class', function(d){
+			return d.type + '-hand';
+		})
+		.attr('x1',0)
+		.attr('y1',function(d){
+			return d.balance ? d.balance : 0;
+		})
+		.attr('x2',0)
+		.attr('y2',function(d){
+			return d.length;
+		})
+		.attr('transform',function(d){
+			return 'rotate('+ d.scale(d.value) +')';
+		});
+
+		updateClockData(data, offset);
+		moveClockHands();
+}
+
+function moveClockHands(){
+	d3.select('#clock-hands').selectAll('line')
+	.data(handData)
+		.transition()
+		.attr('transform',function(d){
+			return 'rotate('+ d.scale(d.value) +')';
+		});
+}
+
+function updateClockData(data, offset){
+	sunrise = convertToUTCDate(data.sunrise, offset);
+	sunset = convertToUTCDate(data.sunset, offset);
+	handData[0].value = sunrise.hours + sunrise.minutes/60;
+	handData[1].value = sunset.hours + sunset.minutes/60;
+}
+
+function loadClockTableGroup(data, offset, svg) {
+	var tableGroup = svg.append('g').attr('id','clock_table');
+
+	var time = {};
+	time.monitor_time = convertToUTCDateString(data.time, offset);
+	time.sunrise_time = convertToUTCDateString(data.sunrise, offset);
+	time.sunset_time = convertToUTCDateString(data.sunset, offset);
+	
+    tableGroup.append("text")
+ 				.attr("x", 15)
+    			.attr("y", clockHeight + 50)
+    			.text('Sunrise Time: ' + time.sunrise_time);
+
+   tableGroup.append("text")
+ 				.attr("x", 15)
+    			.attr("y", clockHeight + 75)
+    			.text('Sunset Time: ' + time.sunset_time);			
+}
+
+function loadClockTable(data, offset) {
+	console.log("adding table");
+	d3.select("body").select("#clock_table").remove();
+	
+	var table = d3.select("body").select("div#clock").append("table").attr('id','clock_table');
+	
+	var time = {};
+	time.monitor_time = convertToUTCDateString(data.time, offset);
+	time.sunrise_time = convertToUTCDateString(data.sunrise, offset);
+	time.sunset_time = convertToUTCDateString(data.sunset, offset);
+
+	//transpose data as array
+	var transposeTimeData = transposeWeatherDataIntoArray(time);
+
+	//get column list
+	var columns = Object.keys(transposeTimeData[0]);
+
+	var thead = table.append("thead");
+	var tbody = table.append("tbody");
+
+	thead.append('tr')
+		  .selectAll('th')
+		  .data(columns).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
+
+	// create a row for each object in the data
+	var rows = tbody.selectAll('tr')
+	  .data(transposeTimeData)
+	  .enter()
+	  .append('tr');
+
+	// create a cell in each row for each column
+	var cells = rows.selectAll('td')
+	  .data(function (row) {
+		return columns.map(function (column) {
+		  return {column: column, value: row[column]};
+		});
+	  })
+	  .enter()
+	  .append('td')
+		.text(function (d) { return d.value; });
+}
+
+function transposeClockDataIntoArray(data) {
+	var tdata = [];
+	var types = Object.keys(data);
+
+	for(i=0; i < types.length; i++) {
+		var type = types[i];
+		var value = data[type];
+		var obj = {};
+		obj.Type = type;
+		obj.Value = value;
+		tdata.push(obj);
+	}
+	console.log('clockDataTransposed', tdata);
+	return tdata;
+}
---
+++ Weather_Dashboard/js/weather_d3.js
@@ -0,0 +1,307 @@
+/**
+ * D3 script for weather data
+ */
+
+var apiid = '0b55e5f12b5583816c9356d808f18249'
+var mapapiid = 'AIzaSyBsJmm8GAuulHcRxYk3X3E5ZYWXObQvEWU';
+
+var weatherData = {};
+weatherData.city = 'Dallas';
+weatherData.country = 'US';
+weatherData.condition = '';
+weatherData.temp_max = 0;
+weatherData.temperature = 0;
+weatherData.temp_min = 0;
+weatherData.pressure = 0;
+weatherData.humidity = 0;
+weatherData.wind_speed = 0;
+weatherData.wind_degree = 0;
+
+units = 'imperial'
+temp_scale = 'Fahrenheit'
+
+var forecastData = {};
+forecastData.city = 'Dallas';
+forecastData.country = 'US';
+forecastData.days = [];
+forecastData.cnt = 0;
+
+function loadWeatherData(cityName, unitSelected) {
+	units = unitSelected;
+
+	d3.select("body").select("div#dashboard").select("svg").remove();
+	var svg = d3.select("body").select("div#dashboard").append("svg").attr('width',490).attr('height',320);
+	var monitor_svg = svg.append("g");
+	var forecast_svg = svg.append("g");
+	
+	d3.json('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=' + units +'&appid=' + apiid, function (jsondata) {
+    
+		console.log("weather jsonData from API", jsondata);
+
+		var coordinate = {};
+		coordinate.lat = jsondata.coord.lat;
+		coordinate.lon = jsondata.coord.lon;
+		console.log('coorindate', coordinate);
+		
+		var clockTime = {};
+		clockTime.time = jsondata.dt;
+		clockTime.sunrise = jsondata.sys.sunrise;
+		clockTime.sunset = jsondata.sys.sunset;
+		console.log('clockTime', clockTime);
+		
+		weatherData.city = jsondata.name;
+		weatherData.country = jsondata.sys.country;
+		weatherData.condition = jsondata.weather[0].main;
+		weatherData.temperature = jsondata.main.temp;
+		weatherData.temp_min = jsondata.main.temp_min;
+		weatherData.temp_max = jsondata.main.temp_max;
+		weatherData.pressure = jsondata.main.pressure;
+		weatherData.humidity = jsondata.main.humidity;
+		weatherData.wind_speed = jsondata.wind.speed;
+		weatherData.wind_degree = jsondata.wind.deg;
+		console.log("weatherData",weatherData);
+		loadWeatherTable(weatherData);
+
+    	loadThermometer(weatherData.temperature, weatherData.temp_min, weatherData.temp_max, units, monitor_svg);
+		
+		displayClockOrTable(clockTime, coordinate, monitor_svg);
+		
+		var img_url = generateImageUrl(12, coordinate);
+    	loadGoogleImage(img_url, monitor_svg);
+    });
+
+    loadForecastData(cityName, forecast_svg);
+
+}
+
+function loadForecastData(cityName, forecast_svg) {
+	d3.json('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=' + units +'&appid=' + apiid, function (jsondata) {
+    
+		console.log("forecast jsondata from API", jsondata);
+
+		forecastData.city = jsondata.city.name;
+		forecastData.country = jsondata.city.country;
+		forecastData.cnt = jsondata.cnt;
+		forecastData.days = [];
+		for (i = 0; i < forecastData.cnt; i++) { 
+			var obj = {};
+			obj.dt = jsondata.list[i].dt;
+			obj.dt_txt = jsondata.list[i].dt_txt;
+			obj.temp = jsondata.list[i].main.temp;
+			forecastData.days.push(obj);
+		}
+
+		console.log("forecastData",forecastData);
+
+// 		loadForecastTable(forecastData.days);
+    	generateAndLoadLineChart(forecastData.days, forecast_svg);
+
+		
+    });
+}
+
+function generateImageUrl(zoom_value, coordinate) {
+    var latlon = coordinate.lat + "," + coordinate.lon;
+
+    return img_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+latlon+'&zoom=' +zoom_value+ '&size=490x340&sensor=false&key=' + mapapiid;
+
+}
+
+function loadGoogleImage(url, dashboard_svg) {
+	console.log("adding image")
+	var svg = dashboard_svg.append("g");
+	svg.attr("id","gmap")
+			.append("image")
+			.attr("width","490")
+			.attr("height","360")
+			.attr("xlink:href", url);
+}
+
+function loadWeatherTable(data) {
+	console.log("adding table");
+
+	//transpose data as array
+	var transposeData = transposeWeatherDataIntoArray(data);
+
+	//get column list
+	var columns = Object.keys(transposeData[0]);
+
+	// remove table before appending new table
+	d3.select("body").select("div#wtable").selectAll("table").remove();
+
+	// add new table
+	var table = d3.select("body").select("div#wtable").append("table");
+
+	var thead = table.append("thead");
+	var tbody = table.append("tbody");
+
+	thead.append('tr')
+		  .selectAll('th')
+		  .data(columns).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
+
+	// create a row for each object in the data
+	var rows = tbody.selectAll('tr')
+	  .data(transposeData)
+	  .enter()
+	  .append('tr');
+
+	// create a cell in each row for each column
+	var cells = rows.selectAll('td')
+	  .data(function (row) {
+		return columns.map(function (column) {
+		  return {column: column, value: row[column]};
+		});
+	  })
+	  .enter()
+	  .append('td')
+		.text(function (d) { return d.value; });
+}
+
+function transposeWeatherDataIntoArray(data) {
+	var tdata = [];
+	var types = Object.keys(data);
+
+	for(i=0; i < types.length; i++) {
+		var type = types[i];
+		var value = data[type];
+		var obj = {};
+		obj.Type = type;
+		obj.Value = value;
+		tdata.push(obj);
+	}
+	console.log('weatherDataTransposed', tdata);
+	return tdata;
+}
+
+
+function loadForecastTable(data) {
+
+	// remove table before appending new table
+	d3.select("body").select("div#ftable").selectAll("table").remove();
+
+	// add new table
+	var table = d3.select("body").select("div#ftable").append("table");
+	
+	var thead = table.append("thead");
+	var tbody = table.append("tbody");
+
+	thead.append('tr')
+		  .selectAll('th')
+		  .data(['dt_txt','temp']).enter()
+		  .append('th')  
+		    .text(function (column) { return column; });
+	// create rows
+    var tr = tbody.selectAll("tr")
+    .data(data).enter().append("tr")
+
+     // creates cells
+   var td = tr.selectAll("td")
+  .data(function(d){return d3.values(d)})
+  .enter().append("td")
+  .text(function(d) {return d})
+   
+ 
+}
+
+function generateAndLoadLineChart(data, dashboard_svg){
+	//use following line to add graph to div#linechart
+	
+	// define dimensions of graph
+	  var m = {top: 0, right: 50, bottom: 200, left: 50}; // margins
+	  var w = 1000 - m.left - m.right; // width
+	  var h = 300 - m.top - m.bottom; // height
+	  
+	  var x_dim_accessor = function(d){return d.dt};
+	  var y_dim_accessor = function(d){return d.temp};
+
+	  var x_range;
+	  var y_range;
+	
+	  // plot with data json array passed, we'll plot with a line (this array represents only the Y values, X will just be the index location)
+
+	    x_range = [
+	      d3.min(data, x_dim_accessor),
+	      d3.max(data, x_dim_accessor)
+	    ];
+
+	    y_range = [
+	      d3.min(data, y_dim_accessor),
+	      d3.max(data, y_dim_accessor)
+	    ];
+
+		var data2 = data.sort(function(a,b){
+	      return x_dim_accessor(a) - x_dim_accessor(b);
+	    });
+	    
+	    render(data2,x_range,y_range,m,w,h,x_dim_accessor,y_dim_accessor, dashboard_svg);
+
+}
+
+function render(data,x_range,y_range,m,w,h,x_dim_accessor,y_dim_accessor, dashboard_svg){
+    
+    // X scale will fit all values from data[] within pixels 0-w
+    var x = d3.scale.linear().domain(x_range).range([0, w]);
+    // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+    var y = d3.scale.linear().domain(y_range).range([h, 0]);
+      // automatically determining max range can work something like this
+      // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+    // create a line function that can convert data[] into x and y points
+    var line = d3.svg.line()
+      // assign the X function to plot our line as we wish
+      .x(function(d,i) { 
+        return x(x_dim_accessor(d)); 
+      })
+      .y(function(d) { 
+        return y(y_dim_accessor(d)); 
+      })
+	  
+	  
+	  
+      // Add an SVG element with the desired dimensions and margin.
+      var graph = dashboard_svg.append("g").attr('id','5-day-chart')
+            .attr("width", w + m.left + m.right)
+            .attr("height", h + m.top + m.bottom)
+          .append("svg:g")
+            .attr("transform", "translate(" + m.left + "," + m.right+ ")");
+
+      graph.append("text").style("font-size", "16px").text("Five Day Forecast");
+	
+	  // get datatime labels for x-axis
+	  var x_labels = data.map(function(d) {return d.dt_txt.substring(0,13);});
+      // create yAxis
+      var x_txt = d3.scale.ordinal()
+    			.domain(x_labels)
+    			.rangePoints([0, w]);
+
+      var xAxis = d3.svg.axis().scale(x_txt);
+//       var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+      // Add the x-axis.
+      graph.append("svg:g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + h + ")")
+            .call(xAxis)
+            .selectAll("text")	
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-90)" 
+                });;
+
+
+      // create left yAxis
+      var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+      // Add the y-axis to the left
+      graph.append("svg:g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(-25,0)")
+            .call(yAxisLeft);
+      
+        // Add the line by appending an svg:path element with the data line we created above
+      // do this AFTER the axes above so that the line is above the tick-lines
+      graph.append("svg:path").attr("d", line(data));
+		
+  }
